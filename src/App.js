a325ff857620416426a008686f1f6e4441223182
BesiBersih13 <abdullahazzam98@mail.ugm.ac.id>
2019-05-07 10:21:35 +0700

    components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
+import {Link} from 'react-router-dom';
+import Main from './components/page/Main';
 
 class App extends Component {
-    
+    render(){
+        return(
+            <div>
+                <Link to ='/home' >Home</Link>&nbsp;&nbsp;
+                <Link to ='/profile' >profile</Link>&nbsp;&nbsp;
+                <Link to ='/portofolio' >Portofolio</Link>&nbsp;&nbsp;
+                <Link to ='/contact' >Contact</Link>&nbsp;&nbsp;
+                <hr/>
+                <Main/>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/components/page/Main.js
+++ src/components/page/Main.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+import Home from './Home';
+import Profile from './Profile';
+import Contact from './Contact';
+import Portofolio from './Portofolio';
 
 class Main extends React.Component{
     render(){
         return(
-            <div>
-                <h1>Main Page</h1>
-            </div>
+            <Switch>
+                <Route exact path="/" component = {Home}/>
+                <Route path="/home" component = {Home}/>
+                <Route path="/profile" component = {Profile}/>
+                <Route path="/contact" component = {Contact}/>
+                <Route path="/portofolio" component = {Portofolio}/>
+            </Switch>
         )
     }
 }
