2d2d1e5c4c2694fef2b3211a87ae9853926ac435
BesiBersih13 <abdullahazzam98@mail.ugm.ac.id>
2019-05-07 10:39:25 +0700

    belum selesai

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, {Component} from 'react';
 import './App.css';
 import {Link} from 'react-router-dom';
 import Main from './components/page/Main';
+import Button from './components/MyAppBar';
 
 class App extends Component {
     render(){
         return(
             <div>
-                <Link to ='/home' >Home</Link>&nbsp;&nbsp;
-                <Link to ='/profile' >profile</Link>&nbsp;&nbsp;
-                <Link to ='/portofolio' >Portofolio</Link>&nbsp;&nbsp;
-                <Link to ='/contact' >Contact</Link>&nbsp;&nbsp;
+                <Link to ='/home' ><Button>Home</Button></Link>&nbsp;&nbsp;
+                <Link to ='/profile' ><Button>profile</Button></Link>&nbsp;&nbsp;
+                <Link to ='/portofolio' ><Button>Portofolio</Button></Link>&nbsp;&nbsp;
+                <Link to ='/contact' ><Button>Contact</Button></Link>&nbsp;&nbsp;
                 <hr/>
                 <Main/>
             </div>
